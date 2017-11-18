e45435a2dadd7cd1ba041df59c95d5ff681fa58b
janellamarieco <janella_marie_co@dlsu.edu.ph>
2017-11-18 23:19:50 +0800

    slice
    + added slice function (getSumSlice) xd

--- Source Code/WDI OLAP/src/util/DBTest.java
+++ Source Code/WDI OLAP/src/util/DBTest.java
@@ -6,8 +6,13 @@ public class DBTest {
 		DBHelper db = new DBHelper();
 
 		//		db.connectToDatabase();
-		String[][] temp = db.getSumByRegion();
+		String[][] temp = db.getSumSlice("Income", "Lower middle");
 		String[] colNames = db.getColNames();
+		
+		for(int i = 0; i < temp.length; i++) {
+			for(int j = 0; j < 2; j++)
+				System.out.println(temp[i][j]);
+		}
 
 		for(int i = 0; i < colNames.length; i++) {
 			System.out.println(colNames[i]);
